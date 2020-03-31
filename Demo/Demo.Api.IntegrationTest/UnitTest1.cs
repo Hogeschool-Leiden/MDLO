5f642ca7b24a92e9a9e16c2623add7daee625633
wim_de_groot <wim_de_groot@icloud.com>
2020-03-31 10:22:48 +0200

    Added Test, Integration Test and Spec projects

---
+++ Demo/Demo.Api.IntegrationTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Demo.Api.IntegrationTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Demo/Demo.Spec/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Demo.Spec
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
