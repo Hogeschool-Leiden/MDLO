732cc6655be65aed579e220a0d021eb5f9266819
Ian Beemsterboer <s1104168@atudent.hsleiden.nl>
2020-04-03 12:42:01 +0200

    Nu ook de yaml file geadd

---
+++ Demo/Demo.Api.ComponentTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Demo.Api.ComponentTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
