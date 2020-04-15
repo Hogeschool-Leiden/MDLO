20038fe7c0b38d1588d67cf0e4986a50a0ab47df
Wim de Groot <34519486+degrootwim@users.noreply.github.com>
2020-04-15 10:08:47 +0200

    Update WeatherForecastController.cs

--- Demo/Demo.Api/Controllers/WeatherForecastController.cs
+++ Demo/Demo.Api/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace Demo.Api.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Blazing"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
