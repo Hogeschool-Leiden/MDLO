d1924f00cdf299aa5cf480d7eb1f30933b2aae5e
Dennis van Beelen <dvbeelen@gmail.com>
2020-06-01 17:32:12 +0200

    changed styling to work for smaller screens

--- src/CompetentieAppFrontend/CompetentieAppFrontend.Api/ClientApp/src/app/modules/modules.component.ts
+++ src/CompetentieAppFrontend/CompetentieAppFrontend.Api/ClientApp/src/app/modules/modules.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 import {CompetenceMatrixComponent} from "../competencies/competence-matrix/competence-matrix.component";
 import {MatTableDataSource, MatTableModule} from "@angular/material/table";
 // @ts-ignore
@@ -9,16 +9,64 @@ import mockJson from './../../assets/mock-data/eindcompetentie-mock.json';
   templateUrl: './modules.component.html',
   styleUrls: ['./modules.component.scss']
 })
-export class ModulesComponent {
+export class ModulesComponent implements OnInit {
   displayedColumns = ['module', 'specialisation', 'period', 'matrix', 'endRequirements'];
   dataSource = new MatTableDataSource(MODULE_DATA);
+  pageWidthInPixels;
+  columnRemoveName: string = 'endRequirements';
+  fullColumnSize: number = 5;
+  showListUnderMatrix: boolean = true;
 
   constructor() {
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onPageResize() {
+    this.pageWidthInPixels = window.innerWidth;
+    this.resizeTableToFitScreen();
+  }
+
+  resizeTableToFitScreen() {
+    if (this.isScreenToSmallToFitTable()) {
+      this.removeColumn();
+      this.addListUnderMatrix();
+    } else {
+      this.addColumn();
+      this.removeListUnderMatrix();
+    }
+  }
+
+  private removeColumn() {
+    if (this.displayedColumns.length >= this.fullColumnSize){
+      this.displayedColumns.splice(this.displayedColumns.length - 1);
+    }
+  }
+
+  private addColumn() {
+    if (this.displayedColumns.length !< this.fullColumnSize){
+      this.displayedColumns.push(this.columnRemoveName);
+    }
+  }
+
+  private addListUnderMatrix() {
+    this.showListUnderMatrix = true;
+  }
+
+  private removeListUnderMatrix() {
+    this.showListUnderMatrix = false;
+  }
+
+  isScreenToSmallToFitTable() {
+    return this.pageWidthInPixels < 1155;
   }
 
   applyFilter($event: KeyboardEvent) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  ngOnInit() {
+    this.onPageResize();
   }
 }
 
@@ -26,7 +74,7 @@ export interface ModuleModel {
   specialisation: string[];
   module: string;
   period: number;
-  matrix:any;//TODO: use matrix
+  matrix: any;//TODO: use matrix
   endRequirements: string[];
 }
 
