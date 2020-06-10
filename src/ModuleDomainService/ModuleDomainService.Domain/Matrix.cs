8525c636c3371860c1bf2e6139bf6bd1ab51e9d0
wim_de_groot <wim_de_groot@icloud.com>
2020-06-10 14:31:32 +0200

    Fix matrix

--- src/ModuleDomainService/ModuleDomainService.Domain/Matrix.cs
+++ src/ModuleDomainService/ModuleDomainService.Domain/Matrix.cs
@@ -4,8 +4,8 @@ namespace ModuleDomainService.Domain
 {
     public class Matrix
     {
-        public List<string> xHeaders{ get; set; }
-        public List<string> yHeaders { get; set; }
+        public List<string> XHeaders{ get; set; }
+        public List<string> YHeaders { get; set; }
         public int[][] Cells { get; set; }
     }
 }
