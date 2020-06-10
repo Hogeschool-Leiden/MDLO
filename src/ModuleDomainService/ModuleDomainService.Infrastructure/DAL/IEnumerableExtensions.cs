f569f724923a7d3534d583cc63fadc9006d732a1
wim_de_groot <wim_de_groot@icloud.com>
2020-06-10 15:19:53 +0200

    Added 0 check on version

--- src/ModuleDomainService/ModuleDomainService.Infrastructure/DAL/IEnumerableExtensions.cs
+++ src/ModuleDomainService/ModuleDomainService.Infrastructure/DAL/IEnumerableExtensions.cs
@@ -6,6 +6,6 @@ namespace ModuleDomainService.Infrastructure.DAL
     internal static class IEnumerableExtensions
     {
         internal static int Version(this IEnumerable<Event> events) =>
-            events.Max(@event => @event.Stream.Version);
+            events?.Max(@event => @event.Stream.Version) ?? 0;
     }
 }
