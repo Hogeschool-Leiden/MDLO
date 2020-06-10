c90b9625fd3f6d7d8fcb696b58a7d42d0d00a1b8
wim_de_groot <wim_de_groot@icloud.com>
2020-06-10 17:40:07 +0200

    Added some defaults

--- src/CompetentieAppFrontend/CompetentieAppFrontend.Services/Commands/CreateStudiefasenCommand.cs
+++ src/CompetentieAppFrontend/CompetentieAppFrontend.Services/Commands/CreateStudiefasenCommand.cs
@@ -1,15 +1,17 @@
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using CompetentieAppFrontend.Domain;
 
 namespace CompetentieAppFrontend.Services.Commands
 {
+    [ExcludeFromCodeCoverage]
     public class CreateStudiefasenCommand
     {
         public long ModuleId { get; set; }
-        public IEnumerable<Specialisatie> VerplichtVoor { get; set; }
-        public IEnumerable<Specialisatie> AanbevolenVoor { get; set; }
-        public IEnumerable<int> PeriodenNummers { get; set; }
+        public IEnumerable<Specialisatie> VerplichtVoor { get; set; } = new List<Specialisatie>();
+        public IEnumerable<Specialisatie> AanbevolenVoor { get; set; } = new List<Specialisatie>();
+        public IEnumerable<int> PeriodenNummers { get; set; } = new List<int>();
         public IEnumerable<Specialisatie> Specialisaties => VerplichtVoor.Union(AanbevolenVoor);
 
         public IEnumerable<Periode> Perioden => PeriodenNummers
