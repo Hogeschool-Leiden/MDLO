704969ca9bd1ae26ba28c1ddfcc3beff74185fda
wim_de_groot <wim_de_groot@icloud.com>
2020-06-10 19:09:55 +0200

    Added log

--- src/CompetentieAppFrontend/CompetentieAppFrontend.Api.Test/ModuleEventListenerTest.cs
+++ src/CompetentieAppFrontend/CompetentieAppFrontend.Api.Test/ModuleEventListenerTest.cs
@@ -3,6 +3,7 @@ using CompetentieAppFrontend.Api.EventListeners;
 using CompetentieAppFrontend.Constants;
 using CompetentieAppFrontend.Services.Abstractions;
 using CompetentieAppFrontend.Services.Events;
+using Microsoft.Extensions.Logging;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Miffy.MicroServices.Events;
 using Moq;
@@ -13,18 +14,20 @@ namespace CompetentieAppFrontend.Api.Test
     public class ModuleEventListenerTest
     {
         private Mock<IModuleEventsDeserializer> _moduleEventsDeserializer;
+        private Mock<ILogger<ModuleEventListener>> _loggerMock;
 
         [TestInitialize]
         public void TestInitialize()
         {
             _moduleEventsDeserializer = new Mock<IModuleEventsDeserializer>();
+            _loggerMock = new Mock<ILogger<ModuleEventListener>>();
         }
 
         [TestMethod]
         public void ModuleGecreeerd_Should_CreateModule_On_ModuleEventsDeserializer()
         {
             // Arrange
-            var eventListener = new ModuleEventListener(_moduleEventsDeserializer.Object);
+            var eventListener = new ModuleEventListener(_moduleEventsDeserializer.Object, _loggerMock.Object);
 
             // Act
             eventListener.ModuleGecreeerd(new ModuleGecreeerd());
@@ -37,7 +40,7 @@ namespace CompetentieAppFrontend.Api.Test
         public void ModuleGecreeerd_Should_Have_EventListenerAttribute()
         {
             // Arrange
-            var eventListener = new ModuleEventListener(_moduleEventsDeserializer.Object);
+            var eventListener = new ModuleEventListener(_moduleEventsDeserializer.Object, _loggerMock.Object);
 
             // Act
             var result = eventListener.GetType().GetMethod(nameof(eventListener.ModuleGecreeerd))
@@ -51,7 +54,7 @@ namespace CompetentieAppFrontend.Api.Test
         public void ModuleGecreeerd_Should_Have_TopicAttribute()
         {
             // Arrange
-            var eventListener = new ModuleEventListener(_moduleEventsDeserializer.Object);
+            var eventListener = new ModuleEventListener(_moduleEventsDeserializer.Object, _loggerMock.Object);
 
             // Act
             var result = eventListener.GetType().GetMethod(nameof(eventListener.ModuleGecreeerd))
@@ -65,7 +68,7 @@ namespace CompetentieAppFrontend.Api.Test
         public void ModuleGecreeerd_Should_Have_TopicAttribute_With_TopicPattern_Of_ModuleGecreeerd()
         {
             // Arrange
-            var eventListener = new ModuleEventListener(_moduleEventsDeserializer.Object);
+            var eventListener = new ModuleEventListener(_moduleEventsDeserializer.Object, _loggerMock.Object);
 
             // Act
             var result = eventListener.GetType().GetMethod(nameof(eventListener.ModuleGecreeerd))
